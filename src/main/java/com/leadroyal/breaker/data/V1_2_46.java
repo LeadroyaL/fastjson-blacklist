2178de04b26f918d1ecbdec81ab7c5e61ac7d5b3
LeadroyaL <243850435@qq.com>
2019-07-12 10:06:16 +0800

    Find two hash

--- src/main/java/com/leadroyal/breaker/data/V1_2_46.java
+++ src/main/java/com/leadroyal/breaker/data/V1_2_46.java
@@ -14,18 +14,18 @@ public class V1_2_46 {
             data.known = new LinkedList<BlackInfo.BlockItem>() {{
                 add(new BlackInfo.BlockItem(-8165637398350707645L, "junit."));
                 add(new BlackInfo.BlockItem(-8083514888460375884L, "org.apache.ibatis.datasource"));
+                add(new BlackInfo.BlockItem(-7921218830998286408L, "org.osjava.sj."));
                 add(new BlackInfo.BlockItem(-7768608037458185275L, "org.apache.log4j."));
                 add(new BlackInfo.BlockItem(-6179589609550493385L, "org.logicalcobwebs."));
                 add(new BlackInfo.BlockItem(-5194641081268104286L, "org.apache.logging."));
                 add(new BlackInfo.BlockItem(-3935185854875733362L, "org.apache.commons.dbcp"));
+                add(new BlackInfo.BlockItem(-2753427844400776271L, "com.ibatis.sqlmap.engine.datasource"));
                 add(new BlackInfo.BlockItem(-1589194880214235129L, "org.jdom."));
                 add(new BlackInfo.BlockItem(1073634739308289776L, "org.slf4j."));
                 add(new BlackInfo.BlockItem(5688200883751798389L, "javassist."));
                 add(new BlackInfo.BlockItem(8389032537095247355L, "org.jaxen."));
             }};
             data.unknown = new LinkedList<BlackInfo.BlockItem>() {{
-                add(new BlackInfo.BlockItem(-7921218830998286408L, ""));
-                add(new BlackInfo.BlockItem(-2753427844400776271L, ""));
                 add(new BlackInfo.BlockItem(7017492163108594270L, ""));
             }};
         }
